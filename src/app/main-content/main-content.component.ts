98afc3980fae3bbf8b739cb849e37157ecaee1d2
Muhammad Talha Naeem <mt8442905@gmail.com>
2024-07-31 22:50:57 +0500

    cd

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,13 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { HeaderComponent } from '../header/header.component';
 import { FeaturedBooksComponent } from '../featured-books/featured-books.component';
 import { AboutSectionComponent } from '../about-section/about-section.component';
+import { FooterComponent } from "../footer/footer.component";
+import { ReviewsComponent } from "../reviews/reviews.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavBarComponent, HeaderComponent, FeaturedBooksComponent, AboutSectionComponent],
+  imports: [NavBarComponent, HeaderComponent, FeaturedBooksComponent, AboutSectionComponent, FooterComponent, ReviewsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
